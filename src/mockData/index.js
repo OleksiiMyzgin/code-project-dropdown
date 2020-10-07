f2c83b03ad424e88269ea26367c5aca63885ef41
OleksiiMyzgin <oleksii.myzgin@flexiana.com>
2020-10-07 09:35:41 +0200

    add fake data

---
+++ src/mockData/index.js
@@ -0,0 +1,62 @@
+export const websites = [
+  {
+    id: 1,
+    selected: false,
+    name: "website.com",
+  },
+  {
+    id: 2,
+    selected: false,
+    name: "webpage.com",
+  },
+  {
+    id: 3,
+    selected: false,
+    name: "page.com",
+  },
+  {
+    id: 4,
+    selected: false,
+    name: "google.com",
+  },
+  {
+    id: 5,
+    selected: false,
+    name: "twitter.com",
+  },
+  {
+    id: 6,
+    selected: false,
+    name: "facebook.com",
+  },
+  {
+    id: 7,
+    selected: false,
+    name: "fake.com",
+  },
+  {
+    id: 8,
+    selected: false,
+    name: "example.com",
+  },
+  {
+    id: 9,
+    selected: false,
+    name: "sport.com",
+  },
+  {
+    id: 10,
+    selected: false,
+    name: "apple.com",
+  },
+  {
+    id: 11,
+    selected: false,
+    name: "user.com",
+  },
+  {
+    id: 12,
+    selected: false,
+    name: "react.com",
+  },
+];
